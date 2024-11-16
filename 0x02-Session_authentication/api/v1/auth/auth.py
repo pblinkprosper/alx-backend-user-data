f42dabdfde6806ad6a05ebd5f5848427692be668
pblinkprosper <pblinkprosper01@gmail.com>
2024-11-16 23:27:11 +0000

    Task 1 update

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """Authentication module for the API.
 """
-import os
 import re
 from typing import List, TypeVar
 from flask import request
@@ -37,10 +36,3 @@ class Auth:
         """Gets the current user from the request.
         """
         return None
-
-    def session_cookie(self, request=None) -> str:
-        """Gets the value of the cookie named SESSION_NAME.
-        """
-        if request is not None:
-            cookie_name = os.getenv('SESSION_NAME')
-            return request.cookies.get(cookie_name)
